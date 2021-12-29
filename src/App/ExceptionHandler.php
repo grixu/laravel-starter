85a530390c54c22295e71275369dc447f1f08caa
Mateusz Gostański <mateusz.gostanski@gmail.com>
2021-12-29 09:23:34 +0100

    fix: minor bugs found by phpstan

--- src/App/ExceptionHandler.php
+++ src/App/ExceptionHandler.php
@@ -7,31 +7,14 @@ use Throwable;
 
 class ExceptionHandler extends Handler
 {
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        //
-    ];
+    protected $dontReport = [];
 
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
     protected $dontFlash = [
         'current_password',
         'password',
         'password_confirmation',
     ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     *
-     * @return void
-     */
     public function register()
     {
         $this->reportable(function (Throwable $e) {
